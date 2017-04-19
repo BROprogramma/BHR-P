b077cd715cdde4f005772b46f39a016728b8301e
dhvenema <d.h.venema@outlook.com>
2017-04-19 15:34:32 +0200

    booronderzoek skeleton

---
+++ config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  editors: [{
+    name: "First Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  },{
+    name: "Second Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  }],
+  shortName: "bro-booronderzoek",
+  github: "https://github.com/BROprogramma/booronderzoek",
+};
