f2c16c534a638919bc8587d978912770642922c3
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2022-02-21 12:51:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,32 @@
 var respecConfig = {
   specStatus: "GN-WV",
   specType: "IM",
+  subtitle: "Bodemkundige boormonsterbeschrijving en boormonsteranalyse",
   //canonicalURI: "WV",
   //publishDate: "2020-03-25",
   maxTocLevel: 4,
+ logos: [{
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }]  ,	
   editors: [
     {
-      name: "TNO Geologische Dienst Nederland",
-      url: "https://www.tno.nl/nl/",
-    },
+      name: "Geonovum",
+      url: "https://www.geonovum.nl", 
+    },  
   ],
-  shortName: "BHR-P",
+  authors: [
+    {
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	}],
+  shortName: "bhr-p",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/BHR-P",
   previousPublishDate: "2020-03-25",
   previousStatus: "GN-VV",
-  //localBiblio: {
-  //  "ISO19136": {
-  //    "href": "https://www.iso.org/standard/32554.html",
-  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-  //    "authors": [""],
-  //    "date": "September 2007",
-  //    "publisher": "International Organization for Standardization",
-  //  },
-  //},
 };
